feee9f53bddd867eead3313f54ed1fea44b077ed
ElishebaW <elisheba.wiggins@priviahealth.com>
2017-11-26 16:41:25 -0500

    Adding sidebar feature tests

--- spec/feature/sidebar_spec.rb
+++ spec/feature/sidebar_spec.rb
@@ -1,14 +1,28 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "sidebar navigation", :type => :feature do
+RSpec.feature 'sidebar navigation', :type => :feature do
 
-  it 'goes to the root page' do
+  before do
     visit '/'
   end
-end
 
-  # it "clicks the clients link on the side bar" do
-  #   visit '/'
-  #   click_link("Clients")
-  #   expect(page).to have_content("Pet Owners")
-  # end
+  it 'clicks the case load link on the side bar' do
+    click_link('Case Load')
+    expect(page).to have_content('Case Load')
+  end
+
+  it 'clicks the case load link on the side bar' do
+    click_link('Analytics')
+    expect(page).to have_content('Practice Performance Analytics')
+  end
+
+  it 'clicks the clients link on the side bar' do
+    click_link('Clients')
+    expect(page).to have_content('Pet Owners')
+  end
+
+  it 'clicks the clients link on the side bar' 
+    click_link('Clients')
+    expect(page).to have_content('Pet Owners')
+  end
+end
